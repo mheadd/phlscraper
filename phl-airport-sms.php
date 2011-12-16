3381a8390f2c25549731f8a9ac9993afc0757ab0
Mark Headd <mheadd@voiceingov.org>
2011-12-16 14:58:02 -0500

    Fixed choices on flight info. ask

--- phl-airport-sms.php
+++ phl-airport-sms.php
@@ -20,7 +20,7 @@ $date = date("m.d.y");
 
 if($currentCall->channel == "VOICE") {
 	say("Thank you for caling my test app.");
-	$flight = ask("Please say or enter your numeric flight number.", array("choices" => "[1-4]", "attempts" => 3, "timeout" => 5));
+	$flight = ask("Please say or enter your numeric flight number.", array("choices" => "[1-4 DIGITS]", "attempts" => 3, "timeout" => 5));
 	$flight_type = ask("Is your flight an arrival or departure?", array("choices" => "arrival, departure", "attempts" => 3, "timeout" => 5));
 	
 	$flight_num = $flight->value;
